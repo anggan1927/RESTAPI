ddf146124616ed1cfcb206b654c84bdfd1cdfb87
Anggan <79433212+anggan1927@users.noreply.github.com>
2022-06-24 16:54:25 +0700

    Update db.js

--- database/db.js
+++ database/db.js
@@ -3,9 +3,9 @@ var __path = process.cwd(),
      { color } = require(__path + '/lib/color.js')
 
 // Connection URL
-var url = 'https://sekha.me';
+var url = 'https://sekha.me/docs';
 try {
-if(url == 'https://sekha.me') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
+if(url == 'https://sekha.me/docs') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
 } catch (e) {
 	return;
 	}
